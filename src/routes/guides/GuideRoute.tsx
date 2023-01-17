2111a8c9cf854926034031c0d07834bbc5943833
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2023-01-17 22:03:38 +0100

    feat: read guide options from url

--- src/routes/guides/GuideRoute.tsx
+++ src/routes/guides/GuideRoute.tsx
@@ -9,13 +9,30 @@ const GuideScreen = () => {
   const pathnames = location.pathname.split("/");
   const name = pathnames[pathnames.length - 1];
 
-  const options = React.useMemo(
-    () => ({
+  const [options, setOptions] = React.useState(() => {
+    const searchParams = new URLSearchParams(location.search.substring(1));
+
+    const ignoredRules = searchParams.has("ignored-rules")
+      ? searchParams
+          .get("ignored-rules")!
+          .split(",")
+          .map((rule) => (isNaN(Number(rule)) ? -1 : Number(rule)))
+          .filter((rule) => rule !== -1)
+      : [];
+
+    const hideOptional = searchParams.has("hide-optional");
+    const hideSafety = searchParams.has("hide-safety");
+    const hideComments = searchParams.has("hide-comments");
+
+    return {
       collapseInstructionGroups: true,
+      hideComments,
       hideInstructionId: true,
-    }),
-    []
-  );
+      hideOptional,
+      hideSafety,
+      ignoredRules,
+    };
+  });
 
   const parseGuide = React.useCallback(
     (data: string): Guider.Guide<Guider.GenericInstruction> => {
