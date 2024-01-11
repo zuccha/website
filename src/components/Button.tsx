98efd63aac678d0bb799d19ea6ae9230e873fedb
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-01-12 00:03:38 +0100

    feat: add SMW projects to the home

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,11 +9,12 @@ import Text from "./Text.tsx";
 interface ButtonProps {
   full?: boolean;
   hideBorder?: boolean;
-  href: string;
+  href?: string;
   icon?: FontAwesomeSvgCore.IconDefinition;
   isActive?: boolean;
   isExternal?: boolean;
   label: string;
+  onClick?: () => void;
 }
 
 const Button = ({
@@ -24,6 +25,7 @@ const Button = ({
   isActive,
   isExternal,
   label,
+  onClick,
 }: ButtonProps) => {
   const className = React.useMemo(() => {
     const classNames = ["button"];
@@ -35,8 +37,24 @@ const Button = ({
 
   const target = isExternal ? "_blank" : undefined;
 
+  const handleMouseDown = React.useMemo(
+    () =>
+      onClick
+        ? (e: React.MouseEvent) => {
+            e.preventDefault();
+            onClick();
+          }
+        : undefined,
+    [onClick]
+  );
+
   return (
-    <a className={className} href={href} target={target}>
+    <a
+      className={className}
+      href={href}
+      onMouseDown={handleMouseDown}
+      target={target}
+    >
       {icon && (
         <ReactFontAwesome.FontAwesomeIcon icon={icon} className="button-icon" />
       )}
--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -3,6 +3,8 @@ import Button from "../components/Button.tsx";
 import Header from "../components/Header.tsx";
 import IconButton from "../components/IconButton.tsx";
 
+type Category = "dark-souls" | "super-mario-world";
+
 const HomeRoute = () => {
   document.title = "zuccha";
 
@@ -28,36 +30,59 @@ const HomeRoute = () => {
         isExternal: true,
         url: "mailto:amedeo.zucchetti@gmail.com",
       },
-      // {
-      //   url: "https://twitter.com/amedeozucchetti",
-      //   icon: FreeBrandsSvgIcons.faTwitter,
-      //   isExternal: true,
-      //   label: "Twitter",
-      // }
     ],
     [window.location.origin]
   );
 
-  const projects = React.useMemo(
-    () => [
-      {
-        isExternal: false,
-        label: "Dark Souls III Guide",
-        url: "/#/guides/dark-souls-3-any-glitchless-sl1",
+  const categories = React.useMemo(
+    () => ({
+      ["dark-souls"]: {
+        name: "Dark Souls",
+        projects: [
+          {
+            isExternal: false,
+            label: "Dark Souls III Guide",
+            url: "/#/guides/dark-souls-3-any-glitchless-sl1",
+          },
+          {
+            isExternal: false,
+            label: "Solaire's Adventures",
+            url: "/#/games/solaires-adventures",
+          },
+        ],
       },
-      {
-        isExternal: false,
-        label: "Solaire's Adventures",
-        url: "/#/games/solaires-adventures",
+      ["super-mario-world"]: {
+        name: "Super Mario World",
+        projects: [
+          {
+            isExternal: false,
+            label: "Byte Converter",
+            url: "/pages/byte_converter.html",
+          },
+          {
+            isExternal: false,
+            label: "Byte Table Editor",
+            url: "/pages/byte_table_editor.html",
+          },
+          {
+            isExternal: true,
+            label: "ROM Hack Manager",
+            url: "https://github.com/zuccha/rom-hack-manager",
+          },
+          {
+            isExternal: true,
+            label: "SMW Resources",
+            url: "https://github.com/zuccha/smw-code",
+          },
+        ],
       },
-      {
-        isExternal: true,
-        label: "ROM Hack Manager",
-        url: "https://github.com/zuccha/rom-hack-manager",
-      },
-    ],
+    }),
     []
   );
+
+  const [selectedCategory, setSelectedCategory] = React.useState<
+    Category | undefined
+  >();
 
   return (
     <div className="home-route">
@@ -76,14 +101,34 @@ const HomeRoute = () => {
           </div>
 
           <div className="home-route-projects">
-            {projects.map((project) => (
-              <Button
-                full
-                href={project.url}
-                isExternal={project.isExternal}
-                label={project.label}
-              />
-            ))}
+            {selectedCategory ? (
+              <>
+                <Button
+                  full
+                  onClick={() => setSelectedCategory(undefined)}
+                  isExternal={false}
+                  label=".."
+                />
+                {categories[selectedCategory].projects.map((project) => (
+                  <Button
+                    full
+                    href={project.url}
+                    isExternal={project.isExternal}
+                    label={project.label}
+                  />
+                ))}
+              </>
+            ) : (
+              Object.entries(categories).map(([id, category]) => (
+                <Button
+                  full
+                  label={category.name}
+                  onClick={() => {
+                    setSelectedCategory(id as Category);
+                  }}
+                />
+              ))
+            )}
           </div>
         </div>
       </div>
